037beec11170c393b38230881352816a612cce60
gutsdk <e.zhuykov.02@bk.ru>
2024-11-28 19:56:12 +0400

    Добавил GUI + связь между приложениями через Flask

--- script.py
+++ script.py
@@ -1,4 +1,7 @@
 import requests
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
 
 file = open("api.txt", "r")
 
@@ -11,15 +14,23 @@ def get_user_data(user_id, fields):
     response = requests.get(f'https://api.vk.com/method/users.get?user_ids={user_id}&fields={fields}&access_token={access_token}&v={api_version}') 
     return response.json()
 
-user_id = '1'
-user_fields = (
-                "activities,about,books,bdate,"
-                "career,connections,contacts,city,domain,education,exports,"
-                "followers_count,has_photo,has_mobile,home_town,"
-                "sex,schools,screen_name,status,verified,games,interests,"
-                "last_seen,maiden_name,"
-                "military,movies,music,nickname,occupation,online,personal,"
-                "relation,relatives,timezone,tv,"
-                "universities,is_verified,counters")
-user_data = get_user_data(user_id, user_fields)
-print(user_data)
+@app.route('/get_user_info', methods=['GET'])
+def get_user_info():
+    user_id = request.args.get('user_id')
+    user_fields = (
+                    "activities,about,books,bdate,"
+                    "career,connections,contacts,city,domain,education,exports,"
+                    "followers_count,has_photo,has_mobile,home_town,"
+                    "sex,schools,screen_name,status,verified,games,interests,"
+                    "last_seen,maiden_name,"
+                    "military,movies,music,nickname,occupation,online,personal,"
+                    "relation,relatives,timezone,tv,"
+                    "universities,is_verified,counters")
+    user_data = get_user_data(user_id, user_fields)
+
+    if "error" in user_data:
+        return jsonify({"error" : user_data["error"]["error_msg"]}), 400
+    return jsonify(user_data)
+    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)  # Сервер будет доступен на http://localhost:5000
---
+++ wpf_gui/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;  // Для парсинга JSON
+using System.Windows;
+
+namespace wpf_gui
+{
+    public partial class MainWindow : Window
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private async void GetUserInfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            string userId = userIdTextBox.Text;
+
+            try
+            {
+                HttpResponseMessage responseMessage = await _httpClient.GetAsync($"http://localhost:5000/get_user_info?user_id={userId}");
+
+                responseMessage.EnsureSuccessStatusCode();
+
+                string responseBody = await responseMessage.Content.ReadAsStringAsync();
+
+                var userData = JsonConvert.DeserializeObject<dynamic>(responseBody);
+
+                if (userData.error != null)
+                {
+                    userInfoTextBlock.Text = $"Error: {userData.error}";
+                }
+                else
+                {
+                    // Выводим информацию о пользователе
+                    userInfoTextBlock.Text = $"Name: {userData.response[0].first_name} {userData.response[0].last_name}\n" +
+                                             $"City: {userData.response[0].city.title}\n" +
+                                             $"Followers: {userData.response[0].followers_count}";
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show($"Request error: {ex.Message}");
+            }
+        }
+    }
+}
